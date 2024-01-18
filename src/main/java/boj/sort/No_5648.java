e16a61bc87a5498c9595a242b07556c1e8e8ca96
yangyechan <yac392045@mokpo.ac.kr>
2024-01-18 18:55:51 +0900

    "Sort" ~ 1/18

---
+++ src/main/java/boj/sort/No_5648.java
@@ -0,0 +1,26 @@
+package boj.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class No_5648 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++) {
+            String str = sc.next();
+            StringBuilder sb = new StringBuilder();
+            sb.append(str);
+            sb.reverse();
+            arr[i] = Long.parseLong(sb.toString());
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
