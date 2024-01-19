1bcf661dcba946b5e8a123c3a7cbfbe02cba404b
yangyechan <yac392045@mokpo.ac.kr>
2024-01-19 21:26:03 +0900

    "Dynamic Programming" ~ 1/19

---
+++ src/main/java/boj/sort/No_7795.java
@@ -0,0 +1,13 @@
+package boj.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class No_7795 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
