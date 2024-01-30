128fcc33ccc113196ae268a75b3518047faf1b2a
yangyechan <yac392045@mokpo.ac.kr>
2024-01-30 23:10:04 +0900

    "Math" ~ 1/30

---
+++ src/main/java/boj/math/No_1978.java
@@ -0,0 +1,28 @@
+package boj.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_1978 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int ans = 0;
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(input[i]);
+            ans += isPrime(num);
+        }
+
+        System.out.println(ans);
+    }
+    static int isPrime(int n) {
+        if (n == 1) return 0;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return 0;
+        }
+        return 1;
+    }
+}
