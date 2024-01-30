5d2475ad4858e703dc8e14d1d100817cbc62aebc
yangyechan <yac392045@mokpo.ac.kr>
2024-01-30 11:08:34 +0900

    "Greedy" ~ 1/29

---
+++ src/main/java/boj/greedy/No_15903.java
@@ -0,0 +1,41 @@
+package boj.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
+import java.util.PriorityQueue;
+
+public class No_15903 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+
+        String[] str = br.readLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            pq.offer(Long.parseLong(str[i]));
+        }
+
+        while (m-- > 0) {
+            long o = pq.poll();
+            long t = pq.poll();
+            long sum = o + t;
+
+            for (int i = 0; i < 2; i++) {
+                pq.offer(sum);
+            }
+        }
+
+        long ans = 0;
+        int size = pq.size();
+        for (int i = 0; i < size; i++) {
+            ans += pq.poll();
+        }
+
+        System.out.println(ans);
+    }
+}
