2e55cf132493c54563d4747d45fcef93170955bb
yangyechan <yac392045@mokpo.ac.kr>
2024-02-07 10:48:53 +0900

    "BinarySearch" ~ 2/6

---
+++ src/main/java/boj/binarySearch/No_14921.java
@@ -0,0 +1,43 @@
+package boj.binarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class No_14921 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+        Arrays.sort(arr);
+
+        int min = Integer.MAX_VALUE;
+        int ans = 0;
+        for (int i = 0; i < n-1; i++) {
+            int st = i+1;
+            int ed = n-1;
+            while (st <= ed) {
+                int mid = (st + ed) / 2;
+                int sum = arr[mid] + arr[i];
+                int tmp = Math.abs(sum);
+
+                if (min > tmp) {
+                    min = tmp;
+                    ans = sum;
+                }
+
+                if (sum >= 0) {
+                    ed = mid - 1;
+                }else {
+                    st = mid + 1;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
---
+++ src/main/java/boj/binarySearch/No_2467.java
@@ -0,0 +1,42 @@
+package boj.binarySearch;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class No_2467 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+
+        int min = Integer.MAX_VALUE;
+        int ml = 0;
+        int mr = 0;
+        for (int i = 0; i < n-1; i++) {
+            int left = i+1;
+            int right = n-1;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                int sum = Math.abs(arr[mid] + arr[i]);
+
+                if (min > sum) {
+                    ml = i;
+                    mr = mid;
+                    min = sum;
+                }
+
+                if (arr[mid] >= -arr[i]) {
+                    right = mid - 1;
+                }else {
+                    left = mid + 1;
+                }
+            }
+        }
+        System.out.println(arr[ml] + " " + arr[mr]);
+    }
+}
