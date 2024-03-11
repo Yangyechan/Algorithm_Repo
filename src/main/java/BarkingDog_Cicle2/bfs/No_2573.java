2cc3a7c241ced753ac92e60ab5a3b24bd77f5ce3
yangyechan <yac392045@mokpo.ac.kr>
2024-03-11 15:33:57 +0900

    "Bfs" ~3/11

--- src/main/java/BarkingDog_Cicle2/bfs/No_2573.java
+++ src/main/java/BarkingDog_Cicle2/bfs/No_2573.java
@@ -3,11 +3,101 @@ package BarkingDog_Cicle2.bfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class No_2573 {
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] ip = br.readLine().split(" ");
+        N = Integer.parseInt(ip[0]);
+        M = Integer.parseInt(ip[1]);
 
+        map = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            String[] str = br.readLine().split(" ");
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(str[j]);
+            }
+        }
+
+        int year = 0;
+        while (true) {
+            int cnt = 0;
+            visited = new boolean[N][M];
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (!visited[i][j] && map[i][j] > 0) {
+                        cnt++;
+                        if (cnt > 1) {
+                            System.out.println(year);
+                            return;
+                        }
+                        BFS(i, j);
+                    }
+                }
+            }
+
+            if (cnt == 0) {
+                System.out.println(0);
+                return;
+            }
+
+            year++;
+            func();
+        }
+    }
+    static void BFS(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                    if (!visited[nx][ny] && map[nx][ny] > 0) {
+                        queue.offer(new int[]{nx, ny});
+                        visited[nx][ny] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    static void func() {
+        int[][] zero = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] > 0) {
+                    for (int k = 0; k < 4; k++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+                        if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                            if (map[nx][ny] == 0) {
+                                zero[i][j]++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] > 0) {
+                    map[i][j] = Math.max(0, map[i][j] - zero[i][j]);
+                }
+            }
+        }
     }
 }
