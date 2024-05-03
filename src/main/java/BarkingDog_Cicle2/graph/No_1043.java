d42ca1b9aec58da9b93e14549e48755bfc562226
yangyechan <yac392045@mokpo.ac.kr>
2024-05-03 12:09:47 +0900

    "Graph" ~5/3

---
+++ src/main/java/BarkingDog_Cicle2/graph/No_1043.java
@@ -0,0 +1,17 @@
+package BarkingDog_Cicle2.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_1043 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] ip = br.readLine().split(" ");
+        int N = Integer.parseInt(ip[0]);
+        int M = Integer.parseInt(ip[1]);
+
+
+    }
+}
