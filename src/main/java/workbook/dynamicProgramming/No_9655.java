4b09dcfbb441c30e847671cc94f5fa85ce132f03
yangyechan <yac392045@mokpo.ac.kr>
2024-05-13 16:16:45 +0900

    "Dynamic Programming" ~5/13

---
+++ src/main/java/workbook/dynamicProgramming/No_9655.java
@@ -0,0 +1,19 @@
+package workbook.dynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_9655 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if (N % 2 == 1) {
+            System.out.println("SK");
+        }else {
+            System.out.println("CY");
+        }
+    }
+}
