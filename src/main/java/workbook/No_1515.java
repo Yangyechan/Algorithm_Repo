a6a2a37945a39d5fe3e79fe8448a90157cc0086f
yangyechan <yac392045@mokpo.ac.kr>
2024-05-19 18:58:26 +0900

    "String" ~5/19

--- src/main/java/workbook/No_1515.java
+++ src/main/java/workbook/No_1515.java
@@ -31,6 +31,32 @@ package workbook;
 //}
 
 
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//
+//public class No_1515 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        String s = br.readLine();
+//        int pt = 0;
+//
+//        int base = 0;
+//        while (base++ <= 30000) {
+//            String tmp = String.valueOf(base);
+//            for (int i = 0; i < tmp.length(); i++) {
+//                if (tmp.charAt(i) == s.charAt(pt))
+//                    pt++;
+//                if (pt == s.length()) {
+//                    System.out.println(base);
+//                    return;
+//                }
+//            }
+//        }
+//    }
+//}
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -39,16 +65,20 @@ public class No_1515 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String s = br.readLine();
-        int pt = 0;
+        String str = br.readLine();
 
+        int pt = 0;
         int base = 0;
-        while (base++ <= 30000) {
+
+        while (base++ <= 40000) {
             String tmp = String.valueOf(base);
+
             for (int i = 0; i < tmp.length(); i++) {
-                if (tmp.charAt(i) == s.charAt(pt))
+                if (tmp.charAt(i) == str.charAt(pt)) {
                     pt++;
-                if (pt == s.length()) {
+                }
+
+                if (str.length() == pt) {
                     System.out.println(base);
                     return;
                 }
